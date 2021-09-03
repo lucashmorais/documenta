e623b3b9731754a014dd5f185a33ba9748bd19be
Lucas Morais <morais.lucas.h@gmail.com>
2021-09-03 11:43:21 -0300

    Create JS functions for fetching and posting UserSequences

--- src/utils.js
+++ src/utils.js
@@ -36,3 +36,28 @@ export function getAvailableUsers(preProcessingFunc = null) {
 		);
 	});
 }
+
+// Function that calls the /api/v1/user_sequence endpoint to create a new UserSequence
+export function createUserSequence(userSequence) {
+	return new Promise((resolve, reject) => {
+		fetch('http://localhost:3123/api/v1/user_sequence', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(userSequence),
+		}).then((response) => response.json().then((userSequence) => resolve(userSequence)));
+	});
+}
+
+// Function that calls the /api/v1/user_sequences endpoint to fetch UserSequences
+// If processID is provided, it will use it as a URL query parameter to only fetch the UserSequences with such processID
+export function getUserSequences(processID) {
+	return new Promise((resolve, reject) => {
+		let url = 'http://localhost:3123/api/v1/user_sequences';
+		if (processID) url += `?processID=${processID}`;
+		fetch(url).then((response) =>
+			response.json().then((userSequences) => resolve(userSequences))
+		);
+	});
+}
