f747f8295de941c570f7ead7307897a1820eb527
Lucas Morais <morais.lucas.h@gmail.com>
2021-09-21 10:42:41 -0300

    Let permission-checking functions use enum constants

--- controllers/permissionChecks.go
+++ controllers/permissionChecks.go
@@ -2,10 +2,17 @@ package controllers
 
 import "github.com/gofiber/fiber/v2"
 
+type PermissionIDValue int
+
+const (
+	FinishReviewSequence   PermissionIDValue = 17
+	FinishApprovalSequence PermissionIDValue = 18
+)
+
 func UserCanFinishReviewSequence(c *fiber.Ctx) bool {
-	return len(CoreGetLoggedUserPermissions(c, 17)) > 0
+	return len(CoreGetLoggedUserPermissions(c, int(FinishReviewSequence))) > 0
 }
 
 func UserCanFinishApprovalSequence(c *fiber.Ctx) bool {
-	return len(CoreGetLoggedUserPermissions(c, 18)) > 0
+	return len(CoreGetLoggedUserPermissions(c, int(FinishApprovalSequence))) > 0
 }
