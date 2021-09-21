3ea1aa7d095b264b9de9b623af56a5a619fef9c3
Lucas Morais <morais.lucas.h@gmail.com>
2021-09-21 18:32:47 -0300

    Start implementing RoutingModal, which should allow the Secretary to conclude or restart a process' review or approval sequence

---
+++ src/RoutingModal.svelte
@@ -0,0 +1,375 @@
+<script>
+	import {
+		Modal,
+		FluidForm,
+		TextArea,
+		TextInput,
+		Grid,
+		Column,
+		Row,
+		Dropdown,
+		Tag,
+		Checkbox
+	} from "carbon-components-svelte"
+	import isEmail from 'validator/es/lib/isEmail';
+	import isStrongPassword from 'validator/es/lib/isStrongPassword';
+	import { translatable } from './translatable.js'
+	import { ContentSwitcher, Switch } from "carbon-components-svelte";
+	
+	import { createEventDispatcher } from 'svelte';
+	import { getAvailableUsers } from './utils.js';
+import { set } from "js-cookie";
+	
+	const dispatch = createEventDispatcher();
+	
+	function signalBackendModification() {
+		dispatch('backendModification');
+	}
+	
+	export let open = false;
+	
+	let formState = {
+		"title": "",
+		"summary": "",
+		"selectedType": 0,
+		"selectedCenter": 0,
+	}
+	
+	let validationIsEnabled = false;
+	
+	$: coreTitleIsInvalid = formState != null && formState.title != null && formState.title == ""
+	$: coreSummaryIsInvalid = formState != null && formState.summary != null && formState.summary == ""
+
+	$: titleIsInvalid = validationIsEnabled && coreTitleIsInvalid
+	$: summaryIsInvalid = validationIsEnabled && coreSummaryIsInvalid
+	
+	$: someInputIsInvalid = coreTitleIsInvalid || coreSummaryIsInvalid
+	
+	export let processExaminationState = "analysis"
+	$: if (processExaminationState == 'analysis') { clearForm() }
+
+	console.log("[RoutingModal::_initialization::translatable]: ", translatable)
+	
+	function getStringSet(_examinationState) {
+		let set = translatable.ptBR.routingModal[_examinationState]
+		console.log("[RoutingModal::getStringSet::set]: ", set)
+		return set
+	}
+
+	$: stringSet = getStringSet(processExaminationState)
+	$: modalHeading = stringSet.modalHeading
+	$: ctxSwitchOpt1 = stringSet.ctxSwitchOption1
+	$: ctxSwitchOpt2 = stringSet.ctxSwitchOption2
+	
+	let ctxSelectedIdx = 0;
+	
+	let purposePromise = null;
+	function updatePurposePromise(ignored) {
+		purposePromise = new Promise((resolve, reject) => {
+			resolve(processExaminationState)			
+		})
+	}
+	$: updatePurposePromise(processExaminationState)
+	
+	export let userInfo = null;
+	
+	function updateFormState(userInfo) {
+		if (userInfo != null) {
+			formState.email = userInfo.email;
+			formState.firstName = userInfo.firstName;
+			formState.lastName = userInfo.lastName;
+			formState.initials = userInfo.initials;
+			formState.roles = userInfo.roles;
+			formState = formState
+		} else {
+			clearForm()
+		}
+	}
+	
+	$: console.log("[ProcessModal::userInfo]: ", userInfo)
+	$: updateFormState(userInfo)
+	
+	function enableValidation() {
+		validationIsEnabled = true;
+	}
+	
+	function disableValidation() {
+		validationIsEnabled = false;
+	}
+	
+	let available_types;
+	let available_centers = [{"id": 0, "text": "Brasília"}, {"id": 1, "text": "João Cachoeira"}];
+	
+	function updateProcessTypes() {
+		return new Promise((resolve, reject) => {
+			fetch("http://localhost:3123/api/v1/process_types").
+				then((response)=>response.json().
+					then(function (types) {
+						available_types = []
+						console.log("[updateProcessTypes::types]: ", types)
+						for (const u of types) {
+							let typeObj = {}
+							typeObj.id = u.ID
+							typeObj.text= u.Name
+							typeObj.description = u.Description
+							console.log(typeObj)
+							available_types.push(typeObj)
+						}
+						resolve(types)
+					})
+				)
+		})
+	}
+	updateProcessTypes();
+	
+	function updateCenters() {
+		return new Promise((resolve, reject) => {
+			fetch("http://localhost:3123/api/v1/centers").
+				then((response)=>response.json().
+					then(function (centers) {
+						available_centers = []
+						console.log("[updateCenters::centers]: ", centers)
+						for (const u of centers) {
+							let centerObj = {}
+							centerObj.id = u.ID
+							centerObj.text= u.Name
+							centerObj.shortName= u.ShortName
+							centerObj.description = u.Description
+							console.log(centerObj)
+							available_centers.push(centerObj)
+						}
+						resolve(centers)
+					})
+				)
+		})
+	}
+	updateCenters();
+	
+	function clearForm() {
+		const keys = Object.keys(formState)
+		for (const key of keys) {
+			if (key == "selectedType" || key == "selectedCenter") {
+				formState[key] = 0
+			}
+			else {
+				formState[key] = ""
+			}
+		}
+		formState = formState
+		disableValidation()
+	}
+	
+	function clearFormDelayed(delay) {
+		setTimeout(clearForm, delay)
+	}
+	
+	async function submitForm() {
+		if (someInputIsInvalid) {
+			console.log("[submitForm:someInputIsInvalid:formState]: ", formState)
+			console.log("[submitForm]: [titleIsInvalid, summaryIsInvalid] = ", [titleIsInvalid, summaryIsInvalid])
+			enableValidation()
+			return
+		}
+		disableValidation()
+		
+		try {     
+			let requestBody;
+			let response;
+			if (processExaminationState == "analysis") {
+				requestBody = JSON.stringify({
+					"title": formState.title,
+					"summary": formState.summary,
+					"typeID": available_types[formState.selectedType].id,
+					"centerID": available_centers[formState.selectedCenter].id,
+					"userSequenceUserIDs": (await selection_sequence_promise).map((user) => user.ID)
+				});
+				
+				console.log("[submitForm:analysis:requestBody]: ", requestBody);
+				
+				response = await fetch('http://localhost:3123/api/v1/process', {
+					method: 'post',
+					
+					body: requestBody,
+					headers: {
+						'Content-type': 'application/json; charset=UTF-8'
+					}
+				}
+				);
+			} else if (processExaminationState == "approval") {
+				requestBody = JSON.stringify({
+					"title": formState.title,
+					"summary": formState.summary,
+					"typeID": available_types[formState.selectedType].id,
+					"centerID": available_centers[formState.selectedCenter].id,
+					"userSequenceUserIDs": (await selection_sequence_promise).map((user) => user.ID)
+				});
+				
+				console.log("[submitForm:approval:requestBody]: ", requestBody);
+				
+				response = await fetch('http://localhost:3123/api/v1/process', {
+					method: 'put',
+					
+					body: requestBody,
+					headers: {
+						'Content-type': 'application/json; charset=UTF-8'
+					}
+				}
+				);
+			} else {
+				return;
+			}
+			
+			if (response.status == 200) {
+				console.log('[ProcessModal::submitForm]: Successfully performed action: ', processExaminationState);
+				open = false;
+				clearForm();
+				// updateRolesTable();
+				signalBackendModification();
+				// fireToastNotification("success", {email: formState.userValue});
+			} else {
+				console.log('[Add role]: Got valid response from server but process registration has failed.')
+				console.log(response)
+				// buildErrorToastFromResponse(response)
+			}
+			
+		} catch(err) {
+			console.error(`Error: ${err}`);
+			return;
+		}
+	}
+	
+	let usersPromise = getAvailableUsers((users) => {
+		for (const user of users) {
+			user.negativePriority = 0;
+		}
+	});
+	
+	let available_users = null;
+	usersPromise.then((users) => {available_users = users});
+	
+	function getSelectionSequencePromise(inner_available_users) {
+		return new Promise((resolve, reject) => {
+			if (inner_available_users == null) {
+				return;
+			}
+			let sequence = inner_available_users.filter((user) => {return user.negativePriority > 0})
+			sequence.sort((a, b) => {return a.negativePriority - b.negativePriority})
+			console.log("[getSelectionSequencePromise::sequence]: ", sequence)
+			resolve(sequence)
+		})
+	}
+	
+	let selection_sequence_promise = null;
+	$: selection_sequence_promise = getSelectionSequencePromise(available_users)
+	
+	let negative_priority_counter = 1;
+	
+	// NOTE: This function was entirely written by Co-pilot.
+	function getSetNegativePriorityClickCallback(user) {
+		return () => {
+			// NOTE: This part was written by me.
+			console.log("[SetNegativePriorityClickCallback::user before modification]: ", user)
+			if (user.negativePriority == 0) {
+				user.negativePriority = negative_priority_counter;
+				negative_priority_counter += 1;
+			} else {
+				user.negativePriority = 0;
+			}
+			console.log("[SetNegativePriorityClickCallback::user after modification]: ", user)
+			available_users = available_users
+		}
+	}
+</script>
+
+<style>
+	p {
+		margin: 1.8em 0 1em 0;
+	}
+	
+	p:first-child {
+		margin-top: 1em
+	}
+
+	h4 {
+		margin-top: 1.5em;
+	}
+
+	h6 {
+		margin: 1.3em 0 0.3em 0;
+		font-weight: 100;
+	}
+	
+	h6:first-of-type {
+		margin-top: 1em;
+	}
+	
+	.userTagsWrapper {
+		/* margin: 1em 0; */
+		margin-left: 0;
+	}
+	
+	.userTagsWrapper > * {
+		margin-left: 0!important;
+	}
+	
+	.userTagsWrapper p {
+		margin-top: 0em;
+		font-weight: 100;
+		font-size: 12px;
+		color: darkgray;
+	}
+</style>
+
+
+<Modal
+	bind:open
+	modalHeading={modalHeading}
+	primaryButtonText="Confirmar"
+	secondaryButtonText="Cancelar"
+	on:click:button--secondary={() => (open = false)}
+	on:open={() => {
+		updateProcessTypes()
+		updateCenters()
+		updateFormState(userInfo)
+	}}
+	on:close={() => {
+		disableValidation()
+		clearFormDelayed(800)
+	}}
+	on:submit={() => {
+		submitForm()
+	}}
+>
+		<p>De que maneira se deverá seguir com o processo?</p>
+		<ContentSwitcher bind:selectedIndex={ctxSelectedIdx}>
+			<Switch text={ctxSwitchOpt1} />
+			<Switch text={ctxSwitchOpt2} />
+		</ContentSwitcher>
+		{#if !(processExaminationState == "approval" && ctxSelectedIdx == 1)}
+			{#await purposePromise then p}
+				{#await usersPromise then users}
+					<p>Qual deverá ser a nova sequência?</p>
+					<h6>Usuários disponíveis</h6>
+					<div class=userTagsWrapper>
+						{#await selection_sequence_promise then sequence}
+							{#each users as user}
+								<Tag type="gray" disabled={user.negativePriority != 0} on:click={getSetNegativePriorityClickCallback(user)}>{user.FirstName} {user.LastName}</Tag>
+							{/each}
+						{/await}
+					</div>
+					<h6>Usuários selecionados</h6>
+					<div class=userTagsWrapper>
+						{#await selection_sequence_promise then sequence}
+							{#if sequence.length > 0}
+								{#each sequence as user}
+									<Tag type="gray" on:click={getSetNegativePriorityClickCallback(user)}>{user.FirstName} {user.LastName}</Tag>
+								{/each}
+							{:else}
+								<p>Nenhum usuário foi selecionado: o processo será gerenciado exclusivamente pelo Secretário.</p>
+							{/if}
+						{/await}
+					</div>
+				{/await}
+			{/await}
+		{/if}
+</Modal>
---
+++ src/translatable.js
@@ -0,0 +1,12 @@
+export let translatable = {};
+
+translatable.ptBR = {};
+translatable.ptBR.routingModal = {};
+translatable.ptBR.routingModal.analysis = {};
+translatable.ptBR.routingModal.approval = {};
+translatable.ptBR.routingModal.analysis.modalHeading = 'Concluir sequência de análise';
+translatable.ptBR.routingModal.approval.modalHeading = 'Concluir sequência de aprovação';
+translatable.ptBR.routingModal.analysis.ctxSwitchOption1 = 'Estender análise';
+translatable.ptBR.routingModal.analysis.ctxSwitchOption2 = 'Iniciar aprovação';
+translatable.ptBR.routingModal.approval.ctxSwitchOption1 = 'Estender aprovação';
+translatable.ptBR.routingModal.approval.ctxSwitchOption2 = 'Ratificar aprovação';
